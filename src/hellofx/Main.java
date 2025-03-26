a6dac7a242e0afc3e75262a1e082446a01efec8c
PrabhathReddy7324 <prabhathbdl@gmail.com>
2025-03-26 11:53:05 +0530

    modified BillingUI.fxml modified Main.java

--- src/hellofx/Main.java
+++ src/hellofx/Main.java
@@ -10,10 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Change the resource to login.fxml for testing the login page
+        // Load the login UI for testing
         Parent root = FXMLLoader.load(getClass().getResource("BillingUI.fxml"));
         primaryStage.setTitle("Login");
-        primaryStage.setScene(new Scene(root, 2944, 1840));
+        primaryStage.setScene(new Scene(root, 2480, 1440));
         primaryStage.show();
     }
 
